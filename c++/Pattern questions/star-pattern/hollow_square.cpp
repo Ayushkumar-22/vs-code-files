9f520f412004bf7b8cfa33d720df244f8efe158c
Ayushkumar-22 <ayushkumar22124@gmail.com>
2025-07-04 16:07:21 +0530

    2way hollow_square

--- c++/Pattern questions/star-pattern/hollow_square.cpp
+++ c++/Pattern questions/star-pattern/hollow_square.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(i>0&&i<n-1 && j>0&&j<n-1){
-                cout <<" ";
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==1||j==1||i==n||j==n){
+                cout <<"*";
             }
             else{
-                cout <<"*";
+                cout <<" ";
             }
         }
         cout <<"\n";
@@ -19,3 +19,27 @@ int main(){
 
     return 0;
 }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             if(i>0&&i<n-1 && j>0&&j<n-1){
+//                 cout <<" ";
+//             }
+//             else{
+//                 cout <<"*";
+//             }
+//         }
+//         cout <<"\n";
+//     }
+
+
+//     return 0;
+// }
