828063c23760069380417abc4d849b092876c0a4
Ayushkumar-22 <ayushkumar22124@gmail.com>
2025-07-06 21:09:21 +0530

    Alphabet_pattern

--- c++/Pattern questions/Alphabet_pattern/rightangle_tringle.cpp
+++ c++/Pattern questions/Alphabet_pattern/rightangle_tringle.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for (int i=65; i<=65+n; i++){
+        for (int j=65; j<=i; j++){
+            cout<<char(j);
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+// 97 -- for small -- a
