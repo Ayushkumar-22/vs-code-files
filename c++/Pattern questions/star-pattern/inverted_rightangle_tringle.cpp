09ec26ef94f722288bcac499086407077b62ccff
Ayushkumar-22 <ayushkumar22124@gmail.com>
2025-07-07 14:34:42 +0530

    alternative

--- c++/Pattern questions/star-pattern/inverted_rightangle_tringle.cpp
+++ c++/Pattern questions/star-pattern/inverted_rightangle_tringle.cpp
@@ -1,11 +1,30 @@
+// # include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     for(int i=1;i<=n;i++){
+//         for (int j=i;j<=n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+    
+//     return 0;
+// }
+
+
+// another way -- 
+
 # include<iostream>
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
-    for(int i=1;i<=n;i++){
-        for (int j=i;j<=n;j++){
+    for(int i=n;i>0;i--){
+        for (int j=i;j>0;j--){
             cout<<"*";
         }
         cout<<endl;
