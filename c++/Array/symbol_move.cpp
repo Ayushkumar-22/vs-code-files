d078090df7ec54cc5c898b42bc057a4b9bc1ee24
Ayushkumar-22 <ayushkumar22124@gmail.com>
2025-07-15 18:10:26 +0530

    All(any)_symbol_move_front

---
+++ c++/Array/symbol_move.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string n;
+    cin>>n;
+    string arr[n.length()];
+    for(int i=0;i<n.length();i++){
+        arr[i]=n[i];
+    }
+    for(int i=0;i<n.length();i++){
+        if((arr[i]=="#")||(arr[i]=="@")||(arr[i]=="$")||(arr[i]=="&")||(arr[i]=="*")||(arr[i]=="!")||(arr[i]=="?")){
+            cout<<arr[i];
+        }
+    }
+    for(int i=0;i<n.length();i++){
+        if((arr[i]=="#")||(arr[i]=="@")||(arr[i]=="$")||(arr[i]=="&")||(arr[i]=="*")||(arr[i]=="!")||(arr[i]=="?")){
+            continue;
+        }
+        else{
+            cout<<arr[i];
+        }
+    }
+
+    return 0;
+}
