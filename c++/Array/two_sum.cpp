b9f67e9debc9a9f4b480503c3433487d161e8ea6
Ayushkumar-22 <ayushkumar22124@gmail.com>
2025-07-22 19:18:51 +0530

    handle except value

--- c++/Array/two_sum.cpp
+++ c++/Array/two_sum.cpp
@@ -9,13 +9,16 @@ int main() {
         cin >> arr[i];
     }
     int count = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n-1; i++) {
         for(int j=i; j<n; j++){
             if(arr[i]+arr[j]==t){
                 cout << i <<" "<< j << endl;
                 count++;
             }
         }
+    }
+    if(count == 0){
+        cout << -1 << endl;
     }
 
     return 0;
