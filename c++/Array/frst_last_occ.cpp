a3c35ebd6128c64d2637e3122e7838801a2db130
Ayushkumar-22 <ayushkumar22124@gmail.com>
2025-07-30 17:45:27 +0530

    1st and last element occurence in array

---
+++ c++/Array/frst_last_occ.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }   
+        cout<<"1st element - "<<arr[0]<<endl;
+        cout<<"last element - "<<arr[n-1];
+    
+
+    return 0;
+}
