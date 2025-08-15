5bf5881e76758aeac2c25b1f3c972e7d94dec899
Ayushkumar-22 <ayushkumar22124@gmail.com>
2025-08-15 12:47:58 +0530

    suffle and array

---
+++ c++/Array/Shuffle_ele_randomly.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            k = arr[i];
+        }
+        // return k;
+    }
+    for(int i = k; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    for(int i = 0; i < k; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
